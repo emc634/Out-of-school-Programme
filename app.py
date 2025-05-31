305eb5270f761a2db75cd2f7dca62980a5dc47e0
bhuvnn <bhuvnnn@gmail.com>
2025-05-31 17:12:24 +0530

    first commit

---
+++ app.py
@@ -0,0 +1,15 @@
+from flask import Flask,render_template
+
+app=Flask(__name__)
+
+@app.route('/')
+def index():
+    return render_template("front.html")
+
+@app.route('/student_signup')
+def student_signup():
+    return render_template('student_signup.html')
+
+
+if __name__ == '__main__':
+    app.run(debug=True,port=5000,host="0.0.0.0")
